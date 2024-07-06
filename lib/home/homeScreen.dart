618f32a9dfa6c6f277ebc45cad5fb103da56cc0e
Yasser Ahmed <87115659+yasser-hammouda@users.noreply.github.com>
2024-07-06 15:29:16 +0300

    Initial Project. Added empty home screen.

--- lib/home/homeScreen.dart
+++ lib/home/homeScreen.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
-  static const string routeName = 'homeScreen';
+  static const String routeName = 'homeScreen';
 
   @override
   Widget build(BuildContext context) {
