3bf5a35579baba5537c342dbdcb6c815dd4c3b8a
Yasser Ahmed <87115659+yasser-hammouda@users.noreply.github.com>
2024-07-16 21:44:21 +0300

    Alomost complete the project

--- lib/home/homeScreen.dart
+++ lib/home/homeScreen.dart
@@ -1,11 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:islami/home/hadeth/hadeth_tab.dart';
+import 'package:islami/home/quran/quran_tab.dart';
+import 'package:islami/home/radio/radio_tab.dart';
+import 'package:islami/home/sebeha/sebha_tab.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:islami/home/settings/settings_tab.dart';
 
-class HomeScreen extends StatelessWidget {
-
+class HomeScreen extends StatefulWidget {
   static const String routeName = 'homeScreen';
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int selectedIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Stack(
+      children: [
+        Image.asset(
+          'assets/images/background.png',
+          width: double.infinity,
+          height: double.infinity,
+          fit: BoxFit.fill,
+        ),
+        Scaffold(
+          appBar: AppBar(
+            title: Text(
+              AppLocalizations.of(context)!.app_title,
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
+          ),
+          bottomNavigationBar: Theme(
+            data: Theme.of(context).copyWith(
+              canvasColor: Theme.of(context).primaryColor,
+            ),
+            child: BottomNavigationBar(
+              currentIndex: selectedIndex,
+              onTap: (index) {
+                selectedIndex = index;
+                setState(() {});
+              },
+              items: [
+                BottomNavigationBarItem(
+                  icon: ImageIcon(AssetImage('assets/images/quranIcon.png')),
+                  label: AppLocalizations.of(context)!.quran,
+                ),
+                BottomNavigationBarItem(
+                    icon: ImageIcon(AssetImage('assets/images/hadethIcon.png')),
+                    label: AppLocalizations.of(context)!.hadeth),
+                BottomNavigationBarItem(
+                    icon: ImageIcon(AssetImage('assets/images/sebhaIcon.png')),
+                    label: AppLocalizations.of(context)!.sebha),
+                BottomNavigationBarItem(
+                    icon: ImageIcon(AssetImage('assets/images/radioIcon.png')),
+                    label: AppLocalizations.of(context)!.radio),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.settings),
+                    label: AppLocalizations.of(context)!.settings),
+              ],
+            ),
+          ),
+          body: tabs[selectedIndex],
+        ),
+      ],
+    );
   }
+
+  List<Widget> tabs = [
+    QuranTab(),
+    HadethTab(),
+    SebhaTab(),
+    RadioTab(),
+    SettingsTab()
+  ];
 }
---
+++ lib/home/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:islami/home/hadeth/hadeth_details_screen.dart';
+import 'package:islami/home/homeScreen.dart';
+import 'package:islami/home/my_theme_data.dart';
+import 'package:islami/home/quran/sura_details_screen.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:islami/providers/app_config_provider.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(ChangeNotifierProvider(
+      create: (context) => AppConfigProvider(), child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var provider = Provider.of<AppConfigProvider>(context);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: HomeScreen.routeName,
+      routes: {
+        HomeScreen.routeName: (context) => HomeScreen(),
+        SuraDetailsScreen.routeName: (context) => SuraDetailsScreen(),
+        HadethDetailsScreen.routeName: (context) => HadethDetailsScreen(),
+      },
+      theme: MyThemeData.lightMode,
+      themeMode: ThemeMode.dark,
+      darkTheme: MyThemeData.darkMode,
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      locale: Locale(provider.appLanuage),
+    );
+  }
+}
